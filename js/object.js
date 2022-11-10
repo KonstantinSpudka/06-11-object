cac228fe1326dcb70f66e552d5291f469b0f20dc
KonstantinSpudka <kspudka@gmail.com>
2022-11-10 23:51:21 +0200

    Add car

--- js/object.js
+++ js/object.js
@@ -1,41 +1,134 @@
-const firstName = "Text";
-const lastName = "Testovich";
-const email = "test@imail.com";
+// const firstName = "Text";
+// const lastName = "Testovich";
+// const email = "test@imail.com";
 
-const obj = {}; // синтаксичний цукор
-const obj2 = Object();
-const obj3 = new Object();
+// const obj = {}; // синтаксичний цукор
+// const obj2 = Object();
+// const obj3 = new Object();
 
-// властивості: рядки, Symbol()
+// // властивості: рядки, Symbol()
+// const user = {
+//   // key:value,
+//   firstName: "Test",
+//   lastName: "Testovich",
+//   email: "test@imail.com",
+//   password: "qwert",
+// };
+
+// console.log("user :>> ", user);
+// // alert(user); // Не віє виводити об'єкти
+
+// // доступ до властивостей
+// // Долавання властивостей
+// user.age = 10;
+// console.log("user :>> ", user);
+
+// // Читання властивостей
+// console.log("user :>> ", user.email);
+
+// // Видалення властивостей
+// delete user.password;
+// console.log("user :>> ", user);
+
+// // Створити об'єкт машини (марка, модель, год, номер, цвет)
+// // Просмотреть об'єкт
+// // Изменить колір
+// // удалити св-во год випуска
+// // Добавити св-во ім'я/призвище влсника
+// // продивитися об'єкт
+
+// const car = {
+//   mark: "Mersedes",
+//   model: "Brabus",
+//   year: 2022, // без лапок це цифри
+//   number: "007",
+//   color: "white",
+// };
+// console.log("car do:>> ", car);
+
+// // Зміна значення властивості
+// car.color = "black";
+// console.log("car after:>> ", car);
+
+// delete car.year;
+// console.log("car :>> ", car);
+
+// car.name = "Bogdan";
+// car.surname = "Hmelnickiy";
+// console.log("car :>> ", car);
+
+// /* Типи посилання і типи значення*/
+// const a = 5;
+// const b = 5;
+// console.log("a===b :>> ", a === b);
+
+// const o1 = {};
+// const o2 = {};
+// console.log("o1===o2 :>> ", o1 === o2);
+
+// const o3 = o1;
+// console.log("o1===o3 :>> ", o1 === o3);
+// o3.name = "property";
+// console.log("o1 :>> ", o1);
+
+// function f() {
+//   o3.lastName = 1;
+// }
+
+// f(o3);
+// console.log("o3 :>> ", o3);
+
+// /* Копіювання об'єкту чогось в об'єкт*/
+// const user1 = Object.assign({}, user);
+// console.log("user1 :>> ", user1);
+// console.log("user === user1 :>> ", user === user1);
+
+// /* Копіювання об'єкту №2 - спрощенний*/
+// const user2 = { ...user };
+// console.log("user2 :>> ", user2);
+// console.log("user === user2 :>> ", user === user2);
+
+/* */
+// const prop = prompt("input property");
+// const value = prompt("input value");
+
+// const obj = {
+//   [prop]: value,
+// };
+// console.log("obj :>> ", obj);
+
 const user = {
   // key:value,
   firstName: "Test",
   lastName: "Testovich",
   email: "test@imail.com",
   password: "qwert",
+  // методи
+  // this той, об'єкт, що стоїть перед крапкою при виклику метода
+  getFullName: function () {
+    // console.log("this :>> ", this === user);
+    // console.log("Hello from function");
+    return `${this.firstName} ${this.lastName}`;
+  },
+  // changeEmail: function (newEmail) {
+  //   this.email = newEmail;
+  // },
+  //2
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  },
 };
 
-console.log("user :>> ", user);
-// alert(user); // Не віє виводити об'єкти
+// function getFullName(user1) {
+//   return `${user1.firstName} ${user1.lastName}`;
+// }
 
-// доступ до властивостей
-// Долавання властивостей
-user.age = 10;
-console.log("user :>> ", user);
-
-// Читання властивостей
-console.log("user :>> ", user.email);
-
-// Видалення властивостей
-delete user.password;
+// console.log("getFullName(user) :>> ", getFullName(user));
+console.log("user.getFullName() :>> ", user.getFullName());
+user.changeEmail("newmail@gmail.com");
 console.log("user :>> ", user);
 
-// Створити об'єкт машини (марка, модель, год, номер, цвет)
-// Просмотреть об'єкт
-// Изменить колір
-// удалити св-во год випуска
-// Добавити св-во ім'я/призвище влсника
-// продивитися об'єкт
+// змінити колір машини
 
 const car = {
   mark: "Mersedes",
@@ -43,16 +136,16 @@ const car = {
   year: 2022, // без лапок це цифри
   number: "007",
   color: "white",
+
+  changeColor(newColor) {
+    this.color = newColor;
+  },
+  changeValue(valName, value) {
+    this[valName] = value;
+  },
 };
-console.log("car :>> ", car);
 
-// Зміна значення властивості
-car.color = "black";
+car.changeColor("yellow");
 console.log("car :>> ", car);
-
-delete car.year;
-console.log("car :>> ", car);
-
-car.name = "Bogdan";
-car.surname = "Hmelnickiy";
+car.changeValue("year", 2000);
 console.log("car :>> ", car);
