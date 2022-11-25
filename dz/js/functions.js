5d064e9d0e2f5f71410992344d1c922a4333d58c
KonstantinSpudka <kspudka@gmail.com>
2022-11-25 17:20:40 +0200

    Add Task 3

--- dz/js/functions.js
+++ dz/js/functions.js
@@ -18,6 +18,9 @@ console.log(isWorkingAgePerson(20));
 
 // 1 ------------------------------
 
+//Реалізувати функцію, яка приймає число і повертає булевське значення,
+//чи воно просте (просте число ділитися без залишку тільки на себе і на одиницю)
+
 function isNumber(number) {
   if (number > 1) {
     for (let i = 2; i < number; i++) {
@@ -34,3 +37,17 @@ console.log(isNumber(7));
 console.log(isNumber(3));
 
 // 2 ------------------------------
+
+// Реалізувати функцію checkMultiplicity, яка приймає два числа і перевіряє,
+// чи ділиться перше на друге націло (без залишку):
+
+function checkMultiplicity(a, b) {
+  return a % b === 0;
+}
+
+console.log(checkMultiplicity(25, 5));
+console.log(checkMultiplicity(15, 5));
+console.log(checkMultiplicity(15, 3));
+console.log(checkMultiplicity(25, 4));
+
+// 3 ---------------------------------
