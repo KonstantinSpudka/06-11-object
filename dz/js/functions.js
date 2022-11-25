1b023b02b5b7a244daf0452bd61625030d84df9e
KonstantinSpudka <kspudka@gmail.com>
2022-11-25 18:16:13 +0200

    Add Task 4

--- dz/js/functions.js
+++ dz/js/functions.js
@@ -41,8 +41,8 @@ console.log(isNumber(3));
 // Реалізувати функцію checkMultiplicity, яка приймає два числа і перевіряє,
 // чи ділиться перше на друге націло (без залишку):
 
-function checkMultiplicity(a, b) {
-  return a % b === 0;
+function checkMultiplicity(y, z) {
+  return y % z === 0;
 }
 
 console.log(checkMultiplicity(25, 5));
@@ -51,3 +51,45 @@ console.log(checkMultiplicity(15, 3));
 console.log(checkMultiplicity(25, 4));
 
 // 3 ---------------------------------
+
+// Перевірка можливості існування трикутника.
+// Реалізувати функцію, яка приймає довжини трикутника;
+// функція повертає true, якщо треугольник можливий, і false, якщо ні
+// ( a + b> c, a + c> b, b + c> a, (a> 0, b> 0, c> 0)).
+
+function triangle(a, b, c) {
+  return a + b > c && b + c > a && c + a > b && a > 0 && b > 0 && c > 0;
+}
+console.log(triangle(2, 3, 4));
+console.log(triangle(2, 0, 4));
+console.log(triangle(-2, 3, 4));
+
+// 4 -------------------------------
+
+//Реалізувати функції розрахунку площі (або поверхні) наступної фігури (тіла):
+// трикутника, прямокутника (або конуса, паралелепіпеда)
+// в залежності від переданих розмірів фігури.
+// Функція повинна повернути обчислене значення.
+// Мінімум для однієї фігури на вибір.
+
+/**
+ *
+ * @param {number} a1 - довжина сторони трикутника
+ * @param {number} h - довжина проведеної до цієї сторони висоти
+ * @returns {number} - площа трикутника
+ */
+const sTriangle = function (a1, h) {
+  return 0.5 * a1 * h;
+};
+console.log("S трикутника => ", sTriangle(3, 4));
+
+/**
+ *
+ * @param {number} a2 - довжина сторони прямокутника
+ * @param {number} b2 - суміжна довжина сторони прямокутника
+ * @returns {number} - площа прямокутника
+ */
+const sRectangle = function (a2, b2) {
+  return a2 * b2;
+};
+console.log("S трикутника => ", sRectangle(3, 4));
